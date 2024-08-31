0968d3da48bbbd88ba5bbf95d0752265e5c25f74
dimascodes <dimasarisp53@gmail.com>
2024-09-01 04:50:39 +0700

    menambahkan root html

---
+++ assets/index.js
@@ -0,0 +1,8 @@
+<html>
+<head>
+</head>
+<body>
+<h1>ini adalah page saya</h1>
+<script src="page.js"></script>
+</body>
+</html>
---
+++ assets/page.js
@@ -0,0 +1,5 @@
+const input = prompt("masukan nama anda");
+
+const haha = input.toLowerCase;
+
+alert(`hai ${haha}, namamu bagus sekaliiii :)`);
