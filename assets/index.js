65311a0d0204d359afa592fb6bab8bc1ee16310e
dimascodes <139526561+dimascodes@users.noreply.github.com>
2024-11-28 01:47:13 +0700

    delete

--- assets/index.js
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-</head>
-<body>
-<h1>ini adalah page saya</h1>
-<script src="page.js"></script>
-</body>
-</html>
--- assets/page.js
+++
@@ -1,5 +0,0 @@
-const input = prompt("masukan nama anda");
-
-const haha = input.toLowerCase;
-
-alert(`hai ${haha}, namamu bagus sekaliiii :)`);
